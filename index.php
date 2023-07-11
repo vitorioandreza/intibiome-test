889e7896a9194e60671abf8e1ec6da6a21e0b218
DrezaVitorio <deza_vitorio@hotmail.com>
2023-07-11 15:44:29 -0300

    ultimos ajustes para envio da tarefa

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
 		
-		<?php $debug = true; ?>
+		<?php $debug = false; ?>
 
         <link rel="preload" as="style" href="<?php echo assetsurl(); ?>index<?php echo !$debug ? '.min' : ''; ?>.css">
 
